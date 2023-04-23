f9f3e7462c42f1cfee8f4541bf30b0c327590d3a
toxicfrank <ngugifrancis930@gmail.com>
2023-04-23 12:01:52 +0100

    practice

--- const_keyword-3.c
+++ const_keyword-3.c
@@ -29,5 +29,36 @@ int main(void)
 	ptr = &l;
 
 	printf("%d\n", *ptr);
+
+/**
+ * if i did not want to change what pointer points to i would
+ * write the code as shown below
+ */
+	int a = 4, b = 6;
+	
+	int *const dr = &a;
+
+/**
+ * This will be an error because u cant change
+ * what the pointer initially pointed to
+ * but one can actually change the interger in a to be another
+ * integer since the code above states that its only the address it points to
+ * should be constant
+ */
+	dr = &b;
+
+/*This will run*/
+	*dr = 19;
+
+/**
+ * in order for the code to not be able to change what fr  points to and
+ * cannot change the value fr points to
+ */
+	int q = 12, w = 3;
+	const int *const fr = &q;
+
+/*Both lines of code below wont be executed*/
+	*fr = 4;
+	fr = &w;
 	return (0);
 }
