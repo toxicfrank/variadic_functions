4514838c903198d934e8e3027b1878b6b1cf3510
toxicfrank <ngugifrancis930@gmail.com>
2023-04-25 09:14:29 +0100

    practice

--- variadicfunctions-2.c
+++ variadicfunctions-2.c
@@ -1,34 +1,56 @@
-#include <stdio.h>
 #include <stdarg.h>
-/*Function prototypes*/
+#include <stdio.h>
+/*Function prototype*/
 int max(int num_args, ...);
-void print(char *placeholder, ...);
+
 /**
- * main - calls the functions below
+ * main - calls the function below
  * Return: always 0
  */
 int main(void)
 {
-    return (0);
+/*storing the returned number in a int data type when calling the function*/
+	int max_num = max(3, 24, 66, 45);
+
+	printf("max num: %d", max_num);
+
+	return (0);
 }
 
 /**
- * max - look for the largest number
- * @num_args: function parameter
+ * max - looking for the max number
+ * @num_args: function that contains number of arguments
  * Return: max value
  */
 int max(int num_args, ...)
 {
-    va_list args;
+	va_list(args);
 
-    va_start(args, num_args);
+	va_start(args, num_args);
 
-    int i;
+	int i;
+	int max = 0;
 
-    for (i = 0; i < num_args; i++)
-    {
-        int x = va_arg(args, int);
-    }
+	for (i = 0; i < num_args; i++)
+	{
+/**
+ * va_arg goes through the integers and pulls them
+ * out so as to be noted in the program
+ */
+		int x = va_arg(args, int);
 
-    va_end(args);
+		if (i == 0)
+		{
+/*max becomes the first value*/
+			max = x;
+		}
+		else if (x > max)
+		{
+/*replaces max when x is lager than the previous x*/
+			max = x;
+		}
+	}
+
+	va_end(args);
+	return (max);
 }
---
+++ variadicfunctions-3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdarg.h>
+/*function protoype*/
+void placeholder(char *placeholder, ...);
+
+/**
+ */
+
+int main(void)
+{
+	return (0);
+}
+
+/**
+ */
+
