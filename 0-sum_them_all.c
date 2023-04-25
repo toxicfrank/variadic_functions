22373e89cffbb9cdcee6b5efb4e04ecb53f6f386
toxicfrank <ngugifrancis930@gmail.com>
2023-04-25 10:20:11 +0100

    alx practice

--- 0-sum_them_all.c
+++ 0-sum_them_all.c
@@ -37,6 +37,9 @@ int sum_them_all(const unsigned int n, ...)
 	int i;
 	int sum = 0;
 
+    if (n == 0)
+        return (0);
+
 	for (i = 0; i < n; i++)
 	{
 		int x = va_arg(args, int);
--- 1-print_numbers.c
+++ 1-print_numbers.c
@@ -7,4 +7,15 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 
 /**
  * main: call the function below
- * 
+ * Return: always 0
+ */
+
+int main(void)
+{
+	print_numners(", ", 4, 0, 98, -1024, 402);
+	return (0);
+}
+
+/**
+ * print_numbers - function that prints numbers followed by a new line
+ * @
