b8ad2f6c662d14b84a39edd7b1fc26ff5b6e1385
toxicfrank <ngugifrancis930@gmail.com>
2023-04-25 10:12:46 +0100

    alx practice

---
+++ 1-print_number.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+/*Function prototype*/
+
+int sum_them_all(const unsigned int n, ...);
+
+/**
+ * main - check the code
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int sum;
+
+	sum = sum_them_all(2, 98, 1024);
+	printf("%d\n", sum);
+	sum = sum_them_all(4, 98, 1024, 402, -1024);
+	printf("%d\n", sum);
+	return (0);
+}
+
+/**
+ * sum_them_all - returns sum of all its parameters
+ * @n: function parameter that indicates the number of arguments that
+ * can be inserted
+ * and the number of arguments is constant
+ * Return: sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list(args);
+
+	va_start(args, n);
+
+	int i;
+	int sum = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+
+		sum += x;
+	}
+
+	va_end(args);
+
+	return (sum);
+}
