76d1286659400d8a768f0d5bd86f7e82e9fa101e
toxicfrank <ngugifrancis930@gmail.com>
2023-04-25 10:52:48 +0100

    alx practice

--- 1-print_numbers.c
+++ 1-print_numbers.c
@@ -12,10 +12,34 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 
 int main(void)
 {
-	print_numners(", ", 4, 0, 98, -1024, 402);
+	print_numbers(",", 4, 0, 98, -1024, 402);
 	return (0);
 }
 
 /**
  * print_numbers - function that prints numbers followed by a new line
- * @
+ * @separator: function parameter that goes through characters
+ * @n: function parameter that goes through
+ * Return: numbers
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list(args);
+
+	va_start(args, n);
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int x = va_arg(args, int);
+
+		printf("%d", x);
+
+		if (i != (n-1) && separator != NULL)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(args);
+}
