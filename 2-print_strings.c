32f52e9183c6948535b0c8399ea4b71d048ab63a
toxicfrank <ngugifrancis930@gmail.com>
2023-04-26 06:42:15 +0100

    practice

--- 2-print_strings.c
+++ 2-print_strings.c
@@ -24,5 +24,25 @@ int main(void)
  */
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	
+	va_list(strings);
+
+	va_start(strings, n);
+
+	int i;
+	char *str;
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(strings, char *);
+
+		if (str == NULL)
+		{
+			printf("(nil)");
+		}
+		if (i != (n - 1) && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	va_end(strings);
 }
